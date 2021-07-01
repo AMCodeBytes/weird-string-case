123c91bac071ede0ef82c045f60e77a5cda47c49
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2021-07-01 13:34:09 +0100

    Kata initial submission

---
+++ index.js
@@ -0,0 +1,24 @@
+function toWeirdCase(text){
+	let result = '';
+	const words = text.split(' ');
+
+	for (let i = 0; i < words.length; i++) {
+		word = words[i];
+
+		for (let i = 0; i < word.length; i++) {
+			if (i%2 === 0) {
+				result += word.slice(i, i+1).toUpperCase();
+			} else {
+				result += word.slice(i, i+1).toLowerCase();
+			}
+		}
+
+		result += ' ';
+	}
+
+	console.log(result.trim());
+	return result.trim();
+}
+
+toWeirdCase("String");//=> returns "StRiNg"
+toWeirdCase("Weird string case");//=> returns "WeIrD StRiNg CaSe"
